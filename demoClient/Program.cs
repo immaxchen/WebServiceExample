16dc19fbca1b9edd067cdea29cc82e9a83dc42df
Max Chen <immaxchen@gmail.com>
2018-09-24 22:57:49 +0800

    first upload

---
+++ demoClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace demoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new demoServiceReference.demoServiceSoapClient();
+            int n = 20;
+            foreach (int f in client.GetFibonacciSequence(n))
+                Console.WriteLine(f);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ demoServer/demoService.asmx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace demoServer
+{
+    [WebService(Namespace = "http://example.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    // [System.Web.Script.Services.ScriptService]
+    public class demoService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public List<int> GetFibonacciSequence(int n)
+        {
+            var sequence = new List<int>() { 0, 1 };
+            for (int i = 2; i < n; i++)
+                sequence.Add(sequence[i - 2] + sequence[i - 1]);
+            return sequence;
+        }
+    }
+}
